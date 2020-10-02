ead41ce02d6d5da37c6f7ec5fb8e8c21552d3d3e
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-10-03 00:31:20 +0800

    checkpoint following create/destroy

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,19 +1,16 @@
-class UsersController < ApplicationController
-  def index
+class FollowingsController < ApplicationController
+
+  def follow(follower, followed)
+    flash.now[:notice] = 'You now follow'
+    @follow = Following.create(follower_id: follower, followed_id: followed)
+    if @follow.save!
+      flash[:notice] = 'You now follow'
+      redirect_to request.refferer
+    end
+  end
+
+  def unfollow
 
   end
 
-  def show
-
-  def create
-
-  end
-
-  def delete
-
-  end
-
-  def destroy
-    
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'users#index'
 
   get 'login', :to => 'sessions#login'
-  post 'sessions/try_login'
+  post 'sessions/try_login'  
   get 'sessions/login'
   get 'sessions/logout'
+  post 'followings/follow'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
