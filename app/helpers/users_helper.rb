94d3420248ce3161b6d4d7fc59d70d91b789a6a9
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-10-09 23:33:03 +0800

    REFACTOR users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,22 +3,19 @@ module UsersHelper
     content = tag(:div, class: 'source-author') 
     content << link_to(user.username, user_path(user))
     content << tag(:br)
-    if current_user != user.id 
-      unless @following.include?(user) 
-        content << link_to('Follow', followings_follow_path(follower_id: current_user, followed_id: user.id), method: :post, class: 'follow-link')
-      end
-      if @following.include?(user)
-        content << link_to('Unfollow', followings_unfollow_path(follower_id: current_user, followed_id: user.id), method: :delete, class: 'unfollow-link')
-      end
-    end
+    content = follow_link(@following, user, content)
     if user.id == current_user
       content << link_to('Edit', edit_user_path(user))
     end
-    content
+    content    
   end
 
   def follow?(following, user)
     content = tag(:span, class: 'follow-links')
+    follow_link(following, user, content)
+  end
+
+  def follow_link(following, user, content)
     if current_user != user.id 
       unless following.include?(user)
         content << link_to('Follow', followings_follow_path(follower_id: current_user, followed_id: user.id), method: :post)
