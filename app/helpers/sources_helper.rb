bf81f9b06c4ab0ff2773f02d751ad35fddb940b2
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-10-09 23:40:03 +0800

    FIX missing div on source index page

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -1,10 +1,11 @@
 module SourcesHelper
   def source_links(source)
-    content = tag(:div, class: 'source-links') 
+    content = content_tag(:div) 
     if source.author.id == current_user
       content << link_to('Show', source)
       content << link_to('Edit', edit_source_path(source))
       content << link_to('Destroy', source, method: :delete, data: { confirm: 'Are you sure?' })
     end
+    content
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     content = tag(:div, class: 'source-author') 
     content << link_to(user.username, user_path(user))
     content << tag(:br)
-    content = follow_link(@following, user, content)
+    follow_link(@following, user, content)
     if user.id == current_user
       content << link_to('Edit', edit_user_path(user))
     end
