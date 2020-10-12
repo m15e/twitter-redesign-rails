86a549df4ac4d9a269bfd008f21480a80d1c49af
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-10-12 21:14:02 +0800

    ADD profile_pic helper to show profile_img across application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_user
     session[:user_id]          
   end
+
+  def profile_img(user)
+    user.photo.attached? ? url_for(user.photo) : 'profile.png'
+  end
 end
