377856c67afc5e6e790a0ae59a66b0ed415cb05a
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-10-14 00:46:07 +0800

    FIX guard clause error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :check_login
 
   def check_login
-    return unless current_user
+    return if current_user
 
     redirect_to(login_path)
     flash[:notice] = 'Please login to continue'
